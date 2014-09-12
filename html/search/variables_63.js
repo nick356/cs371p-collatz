d6c5e676e360d8b9ea5911fc38f1c36ad90f7a4d
Nicholas Carter <nick356@cs.utexas.edu>
2014-09-11 19:52:06 -0500

    This should be the second to last push all of these files should be what is needed to turn in

---
+++ html/search/variables_63.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['cache',['cache',['../Collatz_8c_09_09.html#a1272eb6d3d210f7da48ebb71e2aa50f3',1,'cache():&#160;Collatz.c++'],['../UVaCollatz_8c_09_09.html#a1272eb6d3d210f7da48ebb71e2aa50f3',1,'cache():&#160;UVaCollatz.c++']]],
+  ['cachestat',['cacheStat',['../Collatz_8c_09_09.html#a8334d2336f3bb98dcdcc6dd2b1dd76ff',1,'cacheStat():&#160;Collatz.c++'],['../UVaCollatz_8c_09_09.html#a8334d2336f3bb98dcdcc6dd2b1dd76ff',1,'cacheStat():&#160;UVaCollatz.c++']]]
+];
